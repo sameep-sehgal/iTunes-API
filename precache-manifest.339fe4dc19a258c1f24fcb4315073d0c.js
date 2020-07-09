6d0385d0d2724d7e92cfe7ac1407a9a6c78dd952
sameep-sehgal <62800195+sameep-sehgal@users.noreply.github.com>
2020-07-09 15:51:41 +0530

    Updates

---
+++ precache-manifest.339fe4dc19a258c1f24fcb4315073d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb63a85c6371fd03412b415d1a5a75a0",
+    "url": "/iTunes-API/index.html"
+  },
+  {
+    "revision": "71177c1a5dd2c33cf4ec",
+    "url": "/iTunes-API/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "71177c1a5dd2c33cf4ec",
+    "url": "/iTunes-API/static/js/2.c8e70b85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/iTunes-API/static/js/2.c8e70b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4872abbc22a57fb6814c",
+    "url": "/iTunes-API/static/js/main.13cbc287.chunk.js"
+  },
+  {
+    "revision": "3952c86beb6684737413",
+    "url": "/iTunes-API/static/js/runtime-main.3183ec30.js"
+  }
+]);
